c608186d1fc54bcb29565ea89625a4211dbe9159
Ygor Goulart <ygorgm95@gmail.com>
2022-11-21 16:43:52 -0300

    feat: logic implemented

--- src/app/Components/quizz/quizz.component.ts
+++ src/app/Components/quizz/quizz.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/Data/quizz_hero_or_villain.json';
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
-  styleUrls: ['./quizz.component.sass']
+  styleUrls: ['./quizz.component.sass'],
 })
 export class QuizzComponent implements OnInit {
+  title: string = '';
 
-  constructor() { }
+  questions: any;
+  questionSelected: any;
+
+  answers: string[] = [];
+  answerSelected: string = '';
+
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = false;
+
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    if (quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
 
+      this.questions = quizz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+      
+      this.answers = this.questionSelected.options;
+    }
+  }
 }
