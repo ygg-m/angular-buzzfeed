37d21ba61daaf1bd02e7c037d01c5947ac0964e8
Ygor Goulart <ygorgm95@gmail.com>
2022-11-21 16:44:03 -0300

    feat: logic implemented

--- src/app/Components/quizz/quizz.component.ts
+++ src/app/Components/quizz/quizz.component.ts
@@ -32,8 +32,40 @@ export class QuizzComponent implements OnInit {
 
       this.questionIndex = 0;
       this.questionMaxIndex = this.questions.length;
-      
-      this.answers = this.questionSelected.options;
     }
+  }
+
+  playerChoice(alias: string) {
+    this.answers.push(alias);
+    this.nextStep();
+  }
+
+  async nextStep() {
+    this.questionIndex += 1;
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      const finalAnswer: string = await this.checkResult(this.answers);
+      this.answerSelected =
+        quizz_questions.results[
+          finalAnswer as keyof typeof quizz_questions.results
+        ];
+      this.finished = true;
+    }
+  }
+
+  async checkResult(answers: string[]) {
+    const result = answers.reduce((previous, current, i, arr) => {
+      if (
+        arr.filter((item) => item === previous).length >
+        arr.filter((item) => item === current).length
+      ) {
+        return previous;
+      } else {
+        return current;
+      }
+    });
+    return result;
   }
 }
